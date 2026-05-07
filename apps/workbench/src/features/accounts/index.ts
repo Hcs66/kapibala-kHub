6a4d97964bedbc66b20194fd074bfc63ea254661
阿楚 <achu@achu.io>
2026-05-07 20:16:14 +0800

    refactor(workbench): 为各 feature 模块添加 barrel exports、ConversationPage 使用统一导入路径

---
+++ apps/workbench/src/features/accounts/index.ts
@@ -0,0 +1,1 @@
+export { AccountStatusBar } from './AccountStatusBar'
---
+++ apps/workbench/src/features/messages/index.ts
@@ -0,0 +1,4 @@
+export { MessagePanel } from './MessagePanel'
+export { MessageInput } from './MessageInput'
+export { MessageBubble, TimeSeparator } from './MessageBubble'
+export { MessageSkeleton, ConversationSkeleton } from './MessageSkeleton'
